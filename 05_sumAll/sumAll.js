6f1ff7534ae5adb0f40ae834f3984c75af526822
Imankhadivar <khadivarimanr@gmail.com>
2023-01-23 23:24:32 -0800

    Completed_sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let total = 0;   
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        console.log(Number.isInteger(a));
+        console.log(Number.isInteger(b));
+        return "ERROR"
+    }   else if (a < b){        
+        for(let i = a; i <= b; i++){                
+            total = total+i;   
+        }        
+    } else {    
+        for(let i=b; i<=a; i++){                
+            total = total+i;   
+        }        
+    } 
+    return total;
 };
+console.log(sumAll(25, 2));
 
 // Do not edit below this line
 module.exports = sumAll;
