9b1b84d2ba4591308b0d9e693749782d8ca0ea94
Imankhadivar <khadivarimanr@gmail.com>
2023-01-23 17:37:38 -0800

    completed_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(someArray, ...a ) { 
+    let resultingArray = someArray;   
+        for(let i=0; i<someArray.length; i++){            
+            if (a.includes(someArray[i])){ 
+                resultingArray.splice(i, 1);
+                i--;
+            }
+        }      
+    return resultingArray;    
 };
+console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4));
+
+// removeFromArray(3, [1, 3, 5]);
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
