294485204863cc4b83299b46d02f7aa5d6c65b60
Imankhadivar <khadivarimanr@gmail.com>
2023-01-25 01:13:22 -0800

    Completed_tempConversion.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,25 @@
+// const leapYears = function(year) {
+//     if((year%4 === 0 && year% 100 === 0 && year%400 === 0) || (year%4 === 0 & !(year % 100 === 0)))  {
+//         return true;
+//     } else {
+//         return false;
+//     }    
+// };
+
 const leapYears = function(year) {
-    if((year%4 === 0 && year% 100 === 0 && year%400 === 0) || (year%4 === 0 & !(year % 100 === 0)))  {
+    if(year%100 === 0 && year%400 === 0) {
         return true;
-    } else {
+    } 
+    if(year%100 === 0) {
         return false;
-    }    
-};
+    } 
+    if (year%4 === 0) {
+        return true;
+    }
+    return false;
+}
 console.log(leapYears(700));
+console.log(leapYears(1900));
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = (temp-32)*(5/9);
+  var rounded = Math.round(temp*10)/10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = (temp*9/5)+32;
+  var rounded = Math.round(temp*10)/10;
+  return rounded;
 };
+console.log(convertToFahrenheit(-10));
 
 // Do not edit below this line
 module.exports = {
